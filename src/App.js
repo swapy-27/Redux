95d4cfbb07ba435db0e2673e5c5cad83f6228620
Swapnil <swapniljain240@gmail.com>
2022-06-06 15:40:22 +0530

    adding ui and few components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-
+import Navbar from "./components/Navbar";
+import MovieCard from "./components/MovieCard";
 function App() {
   return (
     <div className="App">
-      hello
+     <Navbar/>
+     <div className="main">
+       <div className="tabs">
+         <div className="tab"></div>
+         <div className="tab"></div>
+       </div>
+       {/* <div className="List">
+         {data.map((movie)=>{
+          return <MovieCard movie = {movie}/>
+         })}
+       </div> */}
+     </div>
     </div>
   );
 }
