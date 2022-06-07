9e34a1b1b6393259775be5603d12c68882d2da22
Swapnil <swapniljain240@gmail.com>
2022-06-07 15:40:02 +0530

    subscribing to state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import Navbar from "./components/Navbar";
 import MovieCard from "./components/MovieCard";
-import {data} from './data'
-import { useEffect } from "react";
-function App(props) {
-  useEffect(()=>{
-    const {store} = props
-
-    store.subscribe(()=>{
-      console.log('updated')
+import { data } from './data'
+import React, { useEffect } from "react";
+import { render } from "@testing-library/react";
+class App extends React.Component {
+  componentDidMount() {
+    const { store } = this.props
+    store.subscribe(() => {
+    
+      this.forceUpdate();
     })
 
     store.dispatch({
-      type:'ADD_MOVIES',
-      movies:data
+      type: 'ADD_MOVIES',
+      movies: data
     })
+  }
 
-  })
-  return (
-    <div className="App">
-     <Navbar/>
-     <div className="main">
-       <div className="tabs">
-         <div className="tab">MOVIES</div>
-         <div className="tab">FAVOURITES</div>
-       </div>
-       <div className="List">
-         {data.map((movie,idx)=>{
-          return <MovieCard movie = {movie} key ={idx}/>
-         })}
-       </div>
-     </div>
-    </div>
-  );
+
+  render() {
+    const movies = this.props.store.getState();
+    
+
+    return (
+
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">MOVIES</div>
+            <div className="tab">FAVOURITES</div>
+          </div>
+          <div className="List">
+            {movies.map((movie, idx) => {
+              return <MovieCard movie={movie} key={idx} />
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
