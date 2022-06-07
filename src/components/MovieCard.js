bd24def72d4e16231e47520b387ae637119ce004
Swapnil <swapniljain240@gmail.com>
2022-06-07 14:03:33 +0530

    created redux store

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ class MovieCard extends React.Component {
 
     render() {
         const {movie} = this.props;
-        console.log(movie)
+       
         return (
             <div className="movie-card">
                 <div className="left">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import  { createStore} from 'redux'
 import App from './App';
+import movies from './reducers';
 
-
+const store = createStore(movies);
+console.log(store)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <App />
