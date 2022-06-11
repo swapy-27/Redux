5ee367221b1e784dfb86c2b675ee557c0fd531e9
Swapnil <swapniljain240@gmail.com>
2022-06-11 16:37:43 +0530

    handling fav and movies button clicks

--- src/App.js
+++ src/App.js
@@ -6,59 +6,63 @@ import { render } from "@testing-library/react";
 import { addFavourites, addMovies } from "./actions";
 
 class App extends React.Component {
-  state={
-    'favPage' : true};
+  state = {
+    'favPage': true
+  };
 
   componentDidMount() {
     const { store } = this.props
     store.subscribe(() => {
-    
+
       this.forceUpdate();
     })
 
     store.dispatch(addMovies(data))
   }
 
-   handleFavouriteBtnClick = ()=>{
-
-      this.setState(this.state,()=>{
-        return {'favPage':true}
-      })
-    }
+  handleFavouriteBtnClick = () => {
 
   
+    this.setState({'favPage':false})
 
-    
+  }
+  handleMovieBtnClick = () => {
+
+ 
+    this.setState({'favPage':true})
+
+  }
+
 
   isMovieFavourite = (movie) => {
 
     const { favourites } = this.props.store.getState();
 
     if (favourites.indexOf(movie) !== -1) {
-        return true
+      return true
     }
     return false
-}
+  }
   render() {
-    const {movies,favourites} = this.props.store.getState();
-    
+    const { movies, favourites } = this.props.store.getState();
+    console.log(movies, favourites, this.state)
     return (
 
       <div className="App">
         <Navbar />
         <div className="main">
           <div className="tabs">
-            <div className="tab">MOVIES</div>
+            <div className="tab" onClick={this.handleMovieBtnClick}>MOVIES</div>
             <div className="tab" onClick={this.handleFavouriteBtnClick}>FAVOURITES</div>
           </div>
           <div className="List">
-            {this.favPage
-            ?movies.map((movie, idx) => {
-              return <MovieCard movie={movie} key={idx} dispatch={this.props.store.dispatch} isFav = {this.isMovieFavourite(movie)} />
-            })
-            :favourites.map((movie, idx) => {
-              return <MovieCard movie={movie} key={idx} dispatch={this.props.store.dispatch} isFav = {this.isMovieFavourite(movie)} />
-            })}
+            {this.state.favPage
+              ? movies.map((movie, idx) => {
+                return <MovieCard movie={movie} key={idx} dispatch={this.props.store.dispatch} isFav={this.isMovieFavourite(movie)} />
+              })
+              : favourites.map((movie, idx) => {
+                return <MovieCard movie={movie} key={idx} dispatch={this.props.store.dispatch} isFav={true}/>
+              })}
           </div>
         </div>
       </div>
